2228a3c4843c44336bda6982a0698ddbccf8c22a
FrancoBolanos <54276760+FrancoBolanos@users.noreply.github.com>
2019-11-28 07:31:50 -0500

    Create Carro.h

---
+++ Carro.h
@@ -0,0 +1,21 @@
+#include "Nodo.h"
+
+class Carro{
+  private:
+  Coordenada Inicio;
+  Coordenada Final;
+  TipoEntero cantNodos;
+  vector<Nodo*> Nodos;
+  public:
+  Carro();
+  virtual ~Carro();
+  void inicializarNodos();
+  void adicionarNodo(Nodo* Nodo1);
+  void setInicio(TipoEntero _x, TipoEntero _y);
+  void setFinal(TipoEntero _x, TipoEntero _y);
+
+  TipoEntero getIniciox();
+  TipoEntero getInicioy();
+  TipoEntero getFinalx();
+  TipoEntero getFinaly();
+};
